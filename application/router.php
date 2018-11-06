a67c17583e26891478b038d293786cac8c19cbae
Stephanie Luangraj <sluangra@iu.edu>
2018-11-06 18:45:43 -0500

    exercise5
    ex5

--- application/router.php
+++ application/router.php
@@ -1,25 +1,52 @@
 <?php
-function autoloadApplication($className) {
-  $fileName = 'application/'.$className.'.class.php';
-  //puttingis_readable instead of file_exists
-  //and is_readable also sees if file exists
-  if(is_readable($fileName)) {
-    require $fileName;
-  }
+session_start();
+function autoloader($class){
+
+		if(file_exists('application/'.strtolower($class).'.php')){
+			//first check the application directory
+			include_once('application/'.strtolower($class).'.php');
+
+		}elseif(file_exists('application/controllers/'.strtolower($class).'.php')){
+			//then check the controller directory
+			include_once('application/controllers/'.strtolower($class).'.php');
+
+		}elseif(file_exists('application/models/'.strtolower($class).'.php')){
+			//finally check the models directory
+			include_once('application/models/'.strtolower($class).'.php');
+
+		}
+
+
 }
-function autoloadModel($className) {
-  $fileName = 'application/models/' . $className . '.class.php';
-  if(is_readable($fileName)) {
-    require $fileName;
-  }
+require_once('application/config.php');
+spl_autoload_register('autoloader');
+require_once('libraries/password.php');
+
+
+
+//grab the path info and break it apart into separate variables
+$paths = explode('/', $_SERVER['PATH_INFO']);
+
+//check the view, if empty set to default view
+if($paths[1] == ''){
+	$view = DEFAULT_VIEW;
+}else{
+	$view = $paths[1];
 }
-function autoloadController($className) {
-  $fileName = 'application/controllers/'.$className .'.class.php';
-  if(is_readable($fileName)) {
-    require $fileName;
-  }
+//check to see if a method is being called and assign the $method variable
+$method = $paths[2];
+
+//check to see if any parameters are passed and assign the $parameters array
+for($i=3;$i < count($paths);$i++){
+	$parameters[] = $paths[$i];
 }
-spl_autoload_register('autoloadApplication');
-spl_autoload_register('autoloadModel');
-spl_autoload_register('autoloadController');
-new controller();
+
+//uppercase the first variable name and append Controller to it. If none, the default controller will load
+$controller = ucfirst($paths[1]).'Controller';
+//instantiate our controller and pass in parameters
+if (class_exists($controller)) {
+    new $controller($view, $method, $parameters);
+} else {
+		new Controller('404');
+}
+?>
--- application0/load.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class load {
-  function view($file_name, $data=null){
-    if (is_array($data)) {
-      extract($data);
-    }
-    include 'views/'.$file_name;
-  }
-}
